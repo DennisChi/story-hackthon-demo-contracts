4ef76a8e30082b244ab4dbde17a6e5882f9fd08b
DennisChi <dennischi99@gmail.com>
2024-10-20 15:43:33 +0800

    story init

---
+++ contracts/PassportRegistrar.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import {IPAssetRegistry} from "./story-protocol/core/registries/IPAssetRegistry.sol";
+import {RegistrationWorkflows} from "./story-protocol/periphery/workflows/RegistrationWorkflows.sol";
+import {ISPGNFT} from "./story-protocol/periphery/interfaces/ISPGNFT.sol";
+
+import {IPPassport} from "./IPPassport.sol";
+
+/// @notice Register an NFT as an IP Account.
+contract PassportRegistrar {
+    IPAssetRegistry public immutable IP_ASSET_REGISTRY;
+    RegistrationWorkflows public immutable REGISTRATION_WORKFLOWS;
+    IPPassport public immutable SIMPLE_NFT;
+    ISPGNFT public immutable SPG_NFT;
+
+    constructor(address ipAssetRegistry, address registrationWorkflows) {
+        IP_ASSET_REGISTRY = IPAssetRegistry(ipAssetRegistry);
+        REGISTRATION_WORKFLOWS = RegistrationWorkflows(registrationWorkflows);
+        SIMPLE_NFT = new IPPassport("Game Plus Passport", "GPP");
+        SPG_NFT = ISPGNFT(
+            REGISTRATION_WORKFLOWS.createCollection(
+                ISPGNFT.InitParams({
+                    name: "Game Plus Passport Collection",
+                    symbol: "GPP",
+                    baseURI: "https://gameplus.com/demo/passport/metadata/",
+                    contractURI: "https://gameplus.com/demo/passport/contracts/",
+                    maxSupply: 100,
+                    mintFee: 0,
+                    mintFeeToken: address(0),
+                    mintFeeRecipient: address(this),
+                    owner: address(this),
+                    mintOpen: true,
+                    isPublicMinting: false
+                })
+            )
+        );
+    }
+
+    /// @notice Mint an IP NFT and register it as an IP Account via Story Protocol core.
+    /// @return ipId The address of the IP Account.
+    /// @return tokenId The token ID of the IP NFT.
+    function mintIp() external returns (address ipId, uint256 tokenId) {
+        tokenId = SIMPLE_NFT.mint(msg.sender);
+        ipId = IP_ASSET_REGISTRY.register(
+            block.chainid,
+            address(SIMPLE_NFT),
+            tokenId
+        );
+    }
+}
---
+++ contracts/story-protocol/periphery/BaseWorkflow.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {IAccessController} from "../core/interfaces/access/IAccessController.sol";
+import {IIPAssetRegistry} from "../core/interfaces/registries/IIPAssetRegistry.sol";
+import {ILicenseRegistry} from "../core/interfaces/registries/ILicenseRegistry.sol";
+import {ILicensingModule} from "../core/interfaces/modules/licensing/ILicensingModule.sol";
+import {ICoreMetadataModule} from "../core/interfaces/modules/metadata/ICoreMetadataModule.sol";
+import {IPILicenseTemplate} from "../core/interfaces/modules/licensing/IPILicenseTemplate.sol";
+
+import {ISPGNFT} from "./interfaces/ISPGNFT.sol";
+import {SPGNFTLib} from "./lib/SPGNFTLib.sol";
+import {Errors} from "./lib/Errors.sol";
+
+/// @title Base Workflow
+/// @notice The base contract for all Story Protocol Periphery workflows.
+abstract contract BaseWorkflow {
+    /// @notice The address of the Access Controller.
+    IAccessController public immutable ACCESS_CONTROLLER;
+
+    /// @notice The address of the Core Metadata Module.
+    ICoreMetadataModule public immutable CORE_METADATA_MODULE;
+
+    /// @notice The address of the IP Asset Registry.
+    IIPAssetRegistry public immutable IP_ASSET_REGISTRY;
+
+    /// @notice The address of the Licensing Module.
+    ILicensingModule public immutable LICENSING_MODULE;
+
+    /// @notice The address of the License Registry.
+    ILicenseRegistry public immutable LICENSE_REGISTRY;
+
+    /// @notice The address of the PIL License Template.
+    IPILicenseTemplate public immutable PIL_TEMPLATE;
+
+    constructor(
+        address accessController,
+        address coreMetadataModule,
+        address ipAssetRegistry,
+        address licenseRegistry,
+        address licensingModule,
+        address pilTemplate
+    ) {
+        // assumes 0 addresses are checked in the child contract
+        ACCESS_CONTROLLER = IAccessController(accessController);
+        CORE_METADATA_MODULE = ICoreMetadataModule(coreMetadataModule);
+        IP_ASSET_REGISTRY = IIPAssetRegistry(ipAssetRegistry);
+        LICENSE_REGISTRY = ILicenseRegistry(licenseRegistry);
+        LICENSING_MODULE = ILicensingModule(licensingModule);
+        PIL_TEMPLATE = IPILicenseTemplate(pilTemplate);
+    }
+
+    /// @notice Check that the caller is authorized to mint for the provided SPG NFT.
+    /// @notice The caller must have the MINTER_ROLE for the SPG NFT or the SPG NFT must has public minting enabled.
+    /// @param spgNftContract The address of the SPG NFT.
+    modifier onlyMintAuthorized(address spgNftContract) {
+        if (
+            !ISPGNFT(spgNftContract).hasRole(
+                SPGNFTLib.MINTER_ROLE,
+                msg.sender
+            ) && !ISPGNFT(spgNftContract).publicMinting()
+        ) revert Errors.Workflow__CallerNotAuthorizedToMint();
+        _;
+    }
+}
